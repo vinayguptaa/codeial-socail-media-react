c07bcfcd4e1ccb4f8b910304079ca7e419d5fa52
Vinay Gupta <vinaygupta@Vinays-MacBook-Air.local>
2021-11-07 19:28:08 +0530

    react-router-dom 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import propTypes from 'prop-types';
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 
 import { fetchPosts } from '../actions/posts';
-import { PostsList } from './';
+import { PostsList, Navbar } from './';
+
+const Login = () => <div>Login</div>;
+const SignUp = () => <div>SignUp</div>;
+const Home = () => <div>Home</div>;
 
 class App extends React.Component {
   componentDidMount() {
@@ -13,61 +18,29 @@ class App extends React.Component {
   render() {
     const { posts } = this.props;
     return (
-      <div>
-        <nav className="nav">
-          <div className="left-div">
-            <img
-              src="https://ninjasfiles.s3.amazonaws.com/0000000000003454.png"
-              alt="logo"
-            />
-          </div>
-          <div className="search-container">
-            <img
-              className="search-icon"
-              src="https://image.flaticon.com/icons/svg/483/483356.svg"
-              alt="search-icon"
-            />
-            <input placeholder="Search" />
+      <Router>
+        <div>
+          <Navbar />
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </ul>
 
-            <div className="search-results">
-              <ul>
-                <li className="search-results-row">
-                  <img
-                    src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
-                    alt="user-dp"
-                  />
-                  <span>John Doe</span>
-                </li>
-                <li className="search-results-row">
-                  <img
-                    src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
-                    alt="user-dp"
-                  />
-                  <span>John Doe</span>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="right-nav">
-            <div className="user">
-              <img
-                src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
-                alt="user-dp"
-                id="user-dp"
-              />
-              <span>John Doe</span>
-            </div>
-            <div className="nav-links">
-              <ul>
-                <li>Log in</li>
-                <li>Log out</li>
-                <li>Register</li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-        <PostsList posts={posts} />
-      </div>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+          {/* <PostsList posts={posts} /> */}
+        </div>
+      </Router>
     );
   }
 }
