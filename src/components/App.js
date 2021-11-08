220e3c02c463a5c2cd337fe41f5fa9081356b235
Vinay Gupta <vinaygupta@Vinays-MacBook-Air.local>
2021-11-08 11:27:38 +0530

    react-router-dom 2 and login UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import propTypes from 'prop-types';
-import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Routes,
+  useLocation,
+} from 'react-router-dom';
 
 import { fetchPosts } from '../actions/posts';
-import { PostsList, Navbar } from './';
+import { Home, Navbar, Page404, Login } from './';
 
-const Login = () => <div>Login</div>;
 const SignUp = () => <div>SignUp</div>;
-const Home = () => <div>Home</div>;
 
 class App extends React.Component {
   componentDidMount() {
@@ -21,22 +25,12 @@ class App extends React.Component {
       <Router>
         <div>
           <Navbar />
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/signup">Signup</Link>
-            </li>
-          </ul>
 
           <Routes>
-            <Route exact path="/" element={<Home />} />
+            <Route path="/" element={<Home posts={posts} />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
+            <Route path="*" element={<Page404 />} />
           </Routes>
           {/* <PostsList posts={posts} /> */}
         </div>
